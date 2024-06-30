444c87ebf6408c0a538eaf9c4e98acc72b6d098c
dashaz333 <zdasa715@gmail.com>
2024-06-30 14:17:50 +0300

    коммит4
    это все, на что меня хватило:(

--- exam.zhukova_darya/regis.js
+++ exam.zhukova_darya/regis.js
@@ -1,23 +1,22 @@
-const registrationForm = document.getElementById('registrationForm');
+const registrationForm = document.getElementById('registrationForm'); // получение ссылки на форму регистрации
 
 registrationForm.addEventListener('submit', (event) => {
-  event.preventDefault(); 
+   event.preventDefault(); 
 
+  // получение значений из полей формы
   const role = document.getElementById('user').value;
-  const login = document.querySelector('input[name="login"]').value; // Используем querySelector
-  const password = document.querySelector('input[name="password"]').value; // Используем querySelector
+  const login = document.querySelector('input[name="login"]').value; 
+  const password = document.querySelector('input[name="password"]').value; 
 
-  let users = JSON.parse(localStorage.getItem('users')) || [];
-  const existingUser = users.find(user => user.login === login);
-
-  if (existingUser) {
+  let users = JSON.parse(localStorage.getItem('users')) || []; // получение данных о пользователях из localStorage или создание пустого массива
+  const existingUser = users.find(user => user.login === login);  // поиск пользователя с таким же логином
+  if (existingUser) {// проверка, существует ли пользователь с таким логином
     alert('Пользователь с таким логином уже существует!');
   } else {
-    const newUser = { role, login, password };
-    users.push(newUser);
-    localStorage.setItem('users', JSON.stringify(users));
-
-    alert('Регистрация прошла успешно!');
-    window.location.href = 'RetailItem.html'; 
+    const newUser = { role, login, password }; // создание нового объекта пользователя
+    users.push(newUser);  // добавление нового пользователя в массив
+    localStorage.setItem('users', JSON.stringify(users)); // сохранение данных пользователей в localStorage
+    alert('Регистрация прошла успешно!'); // вывод сообщения об успешной регистрации
+    window.location.href = 'RetailItem.html'; // переход на страницу RetailItem.html
   }
 });
